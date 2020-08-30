d0d0dda3e7919aa8cc80d08fb07a51ef209a5d5d
Ramdog69 <cramsay@vistaprint.com>
2020-08-29 20:49:25 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ -z "$REDIRECT_TARGET" ]; then
+	echo "Redirect target variable not set (REDIRECT_TARGET)"
+	exit 1
+else
+	# Add http if not set
+	if ! [[ $REDIRECT_TARGET =~ ^https?:// ]]; then
+		REDIRECT_TARGET="http://$REDIRECT_TARGET"
+	fi
+fi
+
+# Default to 80
+LISTEN="80"
+# Listen to PORT variable given on Cloud Run Context
+if [ ! -z "$PORT" ]; then
+	LISTEN="$PORT"
+fi
+
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+	listen ${LISTEN};
+	rewrite ^/(.*)\$ ${REDIRECT_TARGET}\$1 permanent;
+}
+EOF
+
+
+echo "Listening to $LISTEN, Redirecting HTTP requests to ${REDIRECT_TARGET}..."
+
+exec nginx -g "daemon off;"
